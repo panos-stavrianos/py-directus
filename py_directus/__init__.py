f235c7ae255f59c896fbf06b3bd9711a44f85cc2
Panos Stavrianos <pstavrianosmail@gmail.com>
2023-11-13 20:50:17 +0200

    added __init__.py

--- py_directus/__init__.py
+++ py_directus/__init__.py
@@ -3,6 +3,7 @@ from .filter import F
 
 try:
     from .fast_api.auth import HeaderAndCookieBearer
+    from .fast_api.globals import directus_admin, directus_session, directus_auth
     from .fast_api import globals
 except ImportError:
     pass
