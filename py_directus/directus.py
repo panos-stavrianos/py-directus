7f90f8cf8d904fbbabacf798ab7c3c27ef0aea74
Elie Bursztein <github@elie.net>
2024-07-03 12:49:39 -0700

    collection list

--- py_directus/directus.py
+++ py_directus/directus.py
@@ -93,6 +93,19 @@ class Directus:
         await asyncio.gather(*[task.gather_response() for task in self.tasks])
         self.tasks.clear()
 
+
+    async def list_collections(self) -> List[Dict[str, Any]]:
+        """
+        Return the list of collections in the Directus instance.
+
+        see: https://docs.directus.io/reference/system/collections.html#the-collection-object
+
+        """
+        url = f"{self.url}/collections"
+        response = await self.connection.get(url, auth=self.auth)
+        return response.json()['data']
+
+
     def collection(self, collection: Union[Type[BaseModel], str]) -> DirectusRequest:
         """
         Set collection to be used.
